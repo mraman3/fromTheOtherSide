528925926a6e306545f0533ccccd3b8d2aee1d7f
Aman <braich_aman3@yahoo.ca>
2026-01-19 21:59:32 -0500

    sending multiple asset types via path joins

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -2,13 +2,25 @@
 import path from 'path';
 import fs from 'fs/promises';
 import { sendResponse } from './sendRespone.js';
+import { getContentType } from './getContentType.js';
 
 export async function serveStatic(req, res, baseDir) {
     const filePath = path.join(baseDir, 'public', 'index.html');
 
+    const publicDir = path.join(baseDir, 'public');
+    const pathToResource = path.join(
+        publicDir,
+         req.url === '/' ? 'index.html' : req.url);
+
+        
     try {
-        const content = await fs.readFile(filePath);
-        sendResponse(res, 200, 'text/html', content)
+        const content = await fs.readFile(pathToResource);
+
+        const ext = path.extname(pathToResource);
+
+        const contentType = getContentType(ext);
+
+        sendResponse(res, 200, contentType, content)
     } catch (error) {
         console.log(error)
     }
